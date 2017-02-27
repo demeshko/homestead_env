838f84d40549345e45b8cbd3fff81d8a60a5e7af
Michail Shtanko <fractalzombie@icloud.com>
2017-02-27 15:35:56 +0200

    Fix

--- scripts/serve-silverstripe.sh
+++ scripts/serve-silverstripe.sh
@@ -50,7 +50,7 @@ block="server {
 
     location ~ /framework/.*(main|rpc|tiny_mce_gzip)\.php$ {
         fastcgi_keep_conn on;
-        fastcgi_pass   unix:/var/run/php/php7.1-fpm.sock;
+        fastcgi_pass   unix:/var/run/php/vagrant.sock;
         fastcgi_index  index.php;
         fastcgi_param  SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include        fastcgi_params;
